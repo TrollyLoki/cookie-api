a871dbb89032568b3a6ca62cb3e3ae1ec2a1060a
TrollyLoki <trollyloki@gmail.com>
2024-01-24 14:43:05 -0500

    Create Fabric project

---
+++ src/client/java/net/trollyloki/cookiedebugmod/CookieDebugModClient.java
@@ -0,0 +1,10 @@
+package net.trollyloki.cookiedebugmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CookieDebugModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
