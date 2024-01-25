d62da996864709e80ccce43bcbab3197c38fe97f
TrollyLoki <trollyloki@gmail.com>
2024-01-25 17:33:59 -0500

    Improve cookie command
    - Proper feedback when requesting a cookie
    - Nicer formatting for cookie response feedback
    - Payload strings can now contain spaces

--- src/main/java/net/trollyloki/cookiedebugmod/CookieDebugMod.java
+++ src/main/java/net/trollyloki/cookiedebugmod/CookieDebugMod.java
@@ -6,16 +6,25 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.network.packet.s2c.common.CookieRequestS2CPacket;
 import net.minecraft.network.packet.s2c.common.StoreCookieS2CPacket;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.UUID;
 
 import static com.mojang.brigadier.arguments.StringArgumentType.getString;
-import static com.mojang.brigadier.arguments.StringArgumentType.string;
+import static com.mojang.brigadier.arguments.StringArgumentType.greedyString;
 import static net.minecraft.command.argument.EntityArgumentType.getPlayer;
 import static net.minecraft.command.argument.EntityArgumentType.player;
 import static net.minecraft.command.argument.IdentifierArgumentType.getIdentifier;
@@ -28,6 +37,52 @@ public class CookieDebugMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("cookie-debug-mod");
+
+	private record CookieRequest(UUID uuid, Identifier identifier) {
+		private CookieRequest(ServerPlayerEntity player, Identifier identifier) {
+			this(player.getUuid(), identifier);
+		}
+	}
+
+	private static final Map<CookieRequest, Queue<ServerCommandSource>> COOKIE_REQUESTS = new HashMap<>();
+
+	private void requestCookie(ServerPlayerEntity player, Identifier identifier, ServerCommandSource source) {
+		CookieRequest cookieRequest = new CookieRequest(player, identifier);
+		COOKIE_REQUESTS.computeIfAbsent(cookieRequest, k -> new LinkedList<>()).add(source);
+		player.networkHandler.sendPacket(new CookieRequestS2CPacket(identifier));
+	}
+
+	public static void onCookieResponse(ServerPlayerEntity player, Identifier identifier, byte[] payload) {
+		CookieRequest cookieRequest = new CookieRequest(player, identifier);
+		if (!COOKIE_REQUESTS.containsKey(cookieRequest)) {
+			LOGGER.warn("Unexpected cookie response packet received from " + player.getGameProfile().getName());
+			return;
+		}
+
+		Queue<ServerCommandSource> queue = COOKIE_REQUESTS.get(cookieRequest);
+		queue.remove().sendFeedback(() -> {
+
+			MutableText text = Text.literal("Cookie ")
+					.append(Text.of(identifier))
+					.append(Text.literal(" from "))
+					.append(player.getStyledDisplayName())
+					.append(Text.literal(": "));
+			if (payload == null) {
+				text.append(Text.literal("null"));
+			} else {
+				String string = new String(payload, StandardCharsets.UTF_8);
+				text.append(Text.literal(string).styled(style -> style
+						.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, string))
+						.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.translatable("chat.copy.click")))
+				));
+			}
+			return text;
+
+		}, false);
+		if (queue.isEmpty()) {
+			COOKIE_REQUESTS.remove(cookieRequest);
+		}
+	}
 
 	public static final SimpleCommandExceptionType PAYLOAD_TOO_LONG = new SimpleCommandExceptionType(Text.literal("Payload too long"));
 
@@ -46,13 +101,10 @@ public class CookieDebugMod implements ModInitializer {
 									.executes(context -> {
 										ServerPlayerEntity player = getPlayer(context, "player");
 										Identifier identifier = getIdentifier(context, "cookie");
-										player.networkHandler.sendPacket(new CookieRequestS2CPacket(identifier));
-										context.getSource().sendFeedback(() -> Text.literal(
-												"Cookie request packet with identifier: \"" + identifier + "\" sent to " + player.getName()
-										), false);
+										requestCookie(player, identifier, context.getSource());
 										return Command.SINGLE_SUCCESS;
 									})
-									.then(argument("payload", string())
+									.then(argument("payload", greedyString())
 											.executes(context -> {
 												ServerPlayerEntity player = getPlayer(context, "player");
 												Identifier identifier = getIdentifier(context, "cookie");
@@ -63,7 +115,9 @@ public class CookieDebugMod implements ModInitializer {
 												}
 
 												player.networkHandler.sendPacket(new StoreCookieS2CPacket(identifier, payload));
-												context.getSource().sendFeedback(() -> Text.literal("Cookie " + identifier + " stored on ")
+												context.getSource().sendFeedback(() -> Text.literal("Cookie ")
+														.append(Text.of(identifier))
+														.append(Text.literal(" stored on "))
 														.append(player.getStyledDisplayName()), true);
 												return Command.SINGLE_SUCCESS;
 											})
--- src/main/java/net/trollyloki/cookiedebugmod/mixin/CookieResponseMixin.java
+++ src/main/java/net/trollyloki/cookiedebugmod/mixin/CookieResponseMixin.java
@@ -3,12 +3,9 @@ package net.trollyloki.cookiedebugmod.mixin;
 import net.minecraft.network.packet.c2s.common.CookieResponseC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.Identifier;
 import net.trollyloki.cookiedebugmod.CookieDebugMod;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-
-import java.nio.charset.StandardCharsets;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class CookieResponseMixin {
@@ -18,10 +15,7 @@ public class CookieResponseMixin {
 
     //@Override
     public void onCookieResponse(CookieResponseC2SPacket packet) {
-        Identifier identifier = packet.key();
-        byte[] payload = packet.payload();
-        String string = payload == null ? null : new String(payload, StandardCharsets.UTF_8);
-        CookieDebugMod.LOGGER.info("Cookie received from " + player.getName() + ": " + identifier + " = " + string);
+        CookieDebugMod.onCookieResponse(player, packet.key(), packet.payload());
     }
 
 }
